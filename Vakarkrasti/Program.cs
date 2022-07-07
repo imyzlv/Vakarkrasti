ca2358eda07f94bfa6e87bf340ffdb0fb8fc6f8d
Imants Treidis <imants.treidis@gmail.com>
2022-07-07 23:10:33 +0300

    Added basic categories. Inc. views and controllers

--- Vakarkrasti/Program.cs
+++ Vakarkrasti/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
