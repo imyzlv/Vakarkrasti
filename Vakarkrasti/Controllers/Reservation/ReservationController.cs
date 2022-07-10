ceb5d265432a09d4d62a50a7231e24327f911efa
Imants Treidis <imants.treidis@gmail.com>
2022-07-10 13:04:48 +0300

    Add basic reservation form

---
+++ Vakarkrasti/Controllers/Reservation/ReservationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Vakarkrasti.Models;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vakarkrasti.Controllers.Reservation
+{
+    public class ReservationController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<SelectListItem> guestHouses = new List<SelectListItem>
+            {
+                new SelectListItem() { Text = "Izvēlēties...", Value = "Guest house" },
+                new SelectListItem() { Text = "Paula", Value = "Paula" },
+                new SelectListItem() { Text = "Amēlija", Value = "Amēlija" }
+            };
+            ViewBag.GuestHouses = guestHouses;
+            return View("Index");
+        }
+        [HttpGet]
+        public IActionResult CreateReservation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateReservation(Reservations reservation)
+        {
+            List<SelectListItem> guestHouses = new List<SelectListItem>
+            {
+                new SelectListItem() { Text = "Izvēlēties...", Value = "Guest house" },
+                new SelectListItem() { Text = "Paula", Value = "Paula" },
+                new SelectListItem() { Text = "Amēlija", Value = "Amēlija" }
+            };
+            ViewBag.GuestHouses = guestHouses;
+            return View("Index");
+        }
+    }
+}
+
---
+++ Vakarkrasti/Models/Reservation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vakarkrasti.Models
+{
+    public class Reservations
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Vārds, Uzvārds")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "E-pasts")]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime ArrivalDateTime { get; set; }
+
+        [Required]
+        public DateTime DepartureDateTime { get; set; }
+
+        [Required]
+        [Display(Name = "Viesu māja")]
+        public string GuestHouse { get; set; }
+
+        [Required]
+        [Display(Name = "Viesu skaits")]
+        public int NumberOfGuests { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Ierašanās datums")]
+        public string ArrivalDate => ArrivalDateTime.ToString("MM.dd.yyyy");
+
+        [NotMapped]
+        [Display(Name = "Ierašanās laiks")]
+        public string ArrivalTime => ArrivalDateTime.ToString("HH:mm");
+
+        [NotMapped]
+        [Display(Name = "Izrakstīšanās datums")]
+        public string DepartureDate => DepartureDateTime.ToString("MM.dd.yyyy");
+        [NotMapped]
+        [Display(Name = "Izrakstīšanās laiks")]
+        public string DepartureTime => DepartureDateTime.ToString("HH:mm");
+    }
+}
+
